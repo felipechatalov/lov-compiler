7a08c4469898c3f56dda090de993753867f3e81e
Felipe Chatalov <fechatalov@gmail.com>
2023-12-21 11:42:57 -0300

    fixed compiler.c

--- compiler.c
+++ compiler.c
@@ -4,14 +4,16 @@
 
 #include "lexer.c"
 
-
 void main(){
-    const char *content = "#define#import idt;  . ++";
+    //const char *content = "#define#import idt;  . ++\n aaaa 11 'asdas' \"dasdasd\" exec1 1@@3 l@aa 1a2 1.2 1.2.3";
+    const char *content = "#include <stdio.h>\n\nint main(){\n\tprintf(\"Hello World!\\n\" /comment);\n\treturn 0;\n}";
     Lexer lexer = lexer_new(content, strlen(content));
     Token token;
     token = lexer_next(&lexer);
-    while (token.kind != TOKEN_END){
-        printf("%.*s (%s)\n", token.lenght, token.text, token_to_text(token.kind));
+
+    while (token.class != TOKEN_EOF){
+        printf("%.*s (%s)\n", token.lenght, token.text, token_kind_to_text(evaluate_token(token)));
         token = lexer_next(&lexer);
     }
+
 }
