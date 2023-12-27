536a228b27fa957565e16505b5166be8a013273c
Felipe Chatalov <fechatalov@gmail.com>
2023-12-27 02:25:14 -0300

    prettified printf

--- src/compiler.c
+++ src/compiler.c
@@ -4,14 +4,14 @@
 #include "lexer.c"
 
 int main(){
-    const char *content = "#define#import idt;  . ++\n aaaa 11 'asdas' \"dasdasd\" exec1 1@@3 l@aa 1a2 1.2 1.2.3";
-    //const char *content = "#include <stdio.h>\n\nint main(){\n\tprintf(\"Hello World!\\n\" /comment);\n\treturn 0;\n}";
+    //const char *content = "#define#import idt;  . ++\n aaaa 11 'asdas' \"dasdasd\" exec1 1@@3 l@aa 1a2 1.2 1.2.3";
+    const char *content = "#include <stdio.h>\n\nint main(){\n\tprintf(\"Hello World!\\n\");\n\treturn 0;\n}";
     Lexer lexer = lexer_new(content, strlen(content));
     Token token;
     token = lexer_next(&lexer);
 
     while (token.class != TOKEN_EOF){
-        printf("%.*s (%s)\n", token.lenght, token.text, token_kind_to_text(evaluate_token(token)));
+        printf("[Text] = `%.*s`, [Kind] = `%s`\n", token.lenght, token.text, token_kind_to_text(evaluate_token(token)));
         token = lexer_next(&lexer);
     }
 
