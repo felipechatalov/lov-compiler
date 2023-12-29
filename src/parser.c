28157c9d641f8dfb8d2afc862d498e25d950bad5
Felipe Chatalov <fechatalov@gmail.com>
2023-12-29 01:12:01 -0300

    Wrote base plan to symbol table generation

--- src/parser.c
+++ src/parser.c
@@ -12,3 +12,28 @@ Parser* parser_new(Lexer* lexer){
 
 
 
+/*  how the table should look like
+name        data_type       type
+char*       char*           char*
+
+var1        int             variable
+function1   float(return)   function
+var2        string          variable
+*/
+
+/*
+need to add variables, functions names,
+and values to the symbol table
+
+the main ideia is, for each token, check if its class is
+TOKEN_ID and check if its not a keyword
+if both are true, it is a variable or function, add to the symbol table
+*/
+
+/*
+needed functions to work:
+
+-add_symbol
+-get_symbol
+-already_exists
+*/
