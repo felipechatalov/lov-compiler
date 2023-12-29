bc6fcffd35c70607bc5ed02d23d7d98b34c6d597
Felipe Chatalov <fechatalov@gmail.com>
2023-12-29 00:45:08 -0300

    removed code from parser, need to re-do

--- src/include/parser.h
+++ src/include/parser.h
@@ -2,17 +2,12 @@
 #define PARSER_H
 
 #include "lexer.h"
-#include "AST.h"
 
 typedef struct {
     Lexer* lexer;
-    Token* token;
 } Parser;
 
+
 Parser* parser_new(Lexer* lexer);
-
-Token* parser_eat(Parser *parser, int type);
-
-AST* parser_parse(Parser *parser);
 
 #endif
--- src/parser.c
+++ src/parser.c
@@ -2,21 +2,13 @@
 #include "include/parser.h"
 
 #include <stdlib.h>
-
+#include <stdio.h>
 
 Parser* parser_new(Lexer* lexer){
     Parser *parser = malloc(sizeof(Parser));
     parser->lexer = lexer;
-    //parser->token = lexer_next(lexer);
     return parser;
 }
 
-Token* parser_eat(Parser *parser, int type){
 
-    if (parser->token->kind != type){
-        printf("[Parser]: Expected: `%s`, got: `%s`\n", token_kind_to_text(type), token_kind_to_text(parser->token->kind));
-        exit(1);
-    }
 
-    
-}
